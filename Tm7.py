58767036363cf31d25bc7f923c6fbf5f00a45756
MmdFrame <framebyframestudiosup@gmail.com>
2026-02-19 13:35:09 +0330

    Update Tm7.py

--- Tm7.py
+++ Tm7.py
@@ -1,79 +1,90 @@
-class Queue:
-    def __init__(self ,max = 100):
-        self.list=[None]* max
+class CircularQueue:
+    def __init__(self, capacity=100):
+        self.data = [None] * capacity
         self.front = -1
         self.rear = -1
-    def insert(self,x):
-        if self.rear >= len(self.list) -1 :
-            print("Queue is Full")
+
+    def is_empty(self):
+        return self.front == -1
+
+    def is_full(self):
+        return (self.rear + 1) % len(self.data) == self.front
+
+    def enqueue(self, x):
+        if self.is_full():
+            print("Queue is full")
             return
-        if  self.front == -1 :
-            self.front+= 1 
-            self.rear+= 1
-            self.list[list.rear] = x
-            return
-        self.rear+= 1 
-        self.list[self.rear] = x
-    def Del(self):
-        if self.front == -1 :
+
+        if self.is_empty():
+            self.front = 0
+            self.rear = 0
+        else:
+            self.rear = (self.rear + 1) % len(self.data)
+
+        self.data[self.rear] = x
+
+    def dequeue(self):
+        if self.is_empty():
             print("Queue is empty")
-            return     
-        if  self.front == self.rear :
-            k = self.list[self.front]
+            return None
+
+        k = self.data[self.front]
+
+        if self.front == self.rear:  # فقط یک عنصر
             self.front = -1
             self.rear = -1
-            return k
-        k = self.list[self.front]
-        self.front+= 1
+        else:
+            self.front = (self.front + 1) % len(self.data)
+
         return k
     
-
-
-test = Queue(3)
-test.insert(57)
-test.insert(32)
-test.insert(44)
-test.insert(39) #Queue is Full
-test.Del()
-test.insert(39) #Queue is Full
-
-
-
+    
+    
+    
+    
 
 
 class C_Queue:
     def __init__(self, max):
-        self.list = [] * max
+        self.list = [None] * max   # اصلاح شد
         self.front = -1
         self.rear = -1
-    def  insert(self , x):
-        if (self.rear +1) % len(self.list) == self.front:
+
+    def insert(self, x):
+        if self.is_full():
             print("Queue is full")
             return
-        if  self.front == -1:
+
+        if self.front == -1:   # صف خالی است
             self.front = 0
             self.rear = 0
-            self.list[0] = x
-            return
-        self.rear=(self.rear +1) % len(self.list)
+        else:
+            self.rear = (self.rear + 1) % len(self.list)
+
         self.list[self.rear] = x
+
     def delete(self):
-        if self.front == -1:
+        if self.is_empty():
             print("Queue is empty")
-            return
-        if self.front == self.rear:
-            k = self.list[self.front]
+            return None
+
+        k = self.list[self.front]
+
+        if self.front == self.rear:   # فقط یک عنصر
             self.front = -1
             self.rear = -1
-            return k
-        k = self.list[self.front]
-        self.front = (self.front +1) % len (self.list)
+        else:
+            self.front = (self.front + 1) % len(self.list)
+
         return k
+
     def is_empty(self):
         return self.front == -1
-    
+
     def is_full(self):
-        return (self.rear +1) % len (self.list) == self.front
-    
-    
+        return (self.rear + 1) % len(self.list) == self.front
+
+
+
+
 
