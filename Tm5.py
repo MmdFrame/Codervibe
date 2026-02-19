09944736ed6a6d89058ebac650140704a3ec700a
MmdFrame <framebyframestudiosup@gmail.com>
2026-02-19 13:30:49 +0330

    Update Tm5.py

--- Tm5.py
+++ Tm5.py
@@ -1,97 +1,59 @@
-#node گره
-class node :
-    def __init__(self , d):
+class node:
+    def __init__(self, d):
         self.Data = d
         self.next = None
 
 
-
-
-class linked_list :
+class linked_list:
     def __init__(self):
         self.head = None
-    def insert_frist(self , x):
-        if self.head == None:
-            self.head = node(x)
-        else:
-            a = node(x)
-            a.next = self.head
-            self.head = a     
-    def insert_last(self , x):
-        if self.head is None:
-            self.head = node(x)
-        else:
-            a = node(x)
-            c = self.head
-            while c.next:
-                c = c.next 
-            c.next = a
-    def insert_after(self , x, y):
-        if self.head is None:
-            print("list is empty")
-        else:
-            c = self.head
-            while c:
-                if c.Data == x:
-                    a = node(y)
-                    a.next = c.next
-                    c.next = a
-                c = c.next
-            print("not found")
-#رفع ایراد
-#    def insert_after(self , x, y):
-#        if self.head is None:
-#            print("list is empty")
-#        else:
-#            f = True
-#            c = self.head
-#            while c:
-#                if c.Data == x:
-#                    a = node(y)
-#                    a.next = c.next
-#                    c.next = a
-#                    f = False
-#                c = c.next
-#            if flag:
-#                print("not found")
-    def insert_after(self , x, y):
-        if self.head is None:
-            print("list is empty")
-        else:
-            c = self.head
-            while c:
-                if c.Data == x:
-                    a = node(y)
-                    a.next = c.next
-                    c.next = a
-                    return
-                c = c.next
-            print("not found")    
 
-    def insert_after(self , x, y):
+    # درج اول
+    def insert_first(self, x):
+        a = node(x)
+        a.next = self.head
+        self.head = a
+
+    # درج آخر
+    def insert_last(self, x):
+        a = node(x)
         if self.head is None:
-            print("list is empty")
+            self.head = a
             return
-        if self.head.Data == x:
-            self.insert_frist(y)
-            return
+
         c = self.head
         while c.next:
-                if c.next.Data == x:
-                    a = node(y)
-                    a.next = c.next
-                    c.next = a
-                    return
-                c = c.next
-                print("not found")                      
+            c = c.next
+        c.next = a
 
-    def insert_befor(self , x, y):
+    # درج بعد از اولین نودی که Data == x
+    def insert_after(self, x, y):
         if self.head is None:
             print("list is empty")
             return
-        if self.head.Data == x:
-            self.insert_frist(y)
+
+        c = self.head
+        while c:
+            if c.Data == x:
+                a = node(y)
+                a.next = c.next
+                c.next = a
+                return
+            c = c.next
+
+        print("not found")
+
+    # درج قبل از اولین نودی که Data == x
+    def insert_before(self, x, y):
+        if self.head is None:
+            print("list is empty")
             return
+
+        if self.head.Data == x:
+            self.insert_first(y)
+            return
+
+        c = self.head
         while c.next:
             if c.next.Data == x:
                 a = node(y)
@@ -99,5 +61,5 @@ class linked_list :
                 c.next = a
                 return
             c = c.next
+
         print("not found")
-                                                                                                                                              
